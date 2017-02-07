d75a50ea44ef79807355bb527af50e51cab5ff7d
Bill Lytton <billl@neurosim.downstate.edu>
2017-02-06 21:44:25 -0500

    loads but not running correctly

--- HHmunge.py
+++ HHmunge.py
@@ -1,37 +1,36 @@
 from neuron import h
+import axonA
 vvec = [h.Vector() for i in range(3)]
 svec, ivec = h.Vector(), h.Vector()
 ncl =  []
 
-soma=h.Section()
-soma.nseg=100
-soma.L=1e3
-soma.diam=10
-soma.insert('hh')
-soma.gnabar_hh, soma.gkbar_hh, soma.gl_hh, soma.el_hh = 0.12, 0.036, 3e-4, -54.3  # set for all segments with underscore notation
+axon=axonA.AxonA()
+axon.axon.nseg=100
+axon.axon.insert('hh')
+axon.axon.gnabar_hh, axon.axon.gkbar_hh, axon.axon.gl_hh, axon.axon.el_hh = 0.12, 0.036, 3e-4, -54.3  # set for all segments with underscore notation
 
 stim=h.IClamp()
-stim.loc(0.0,sec=soma)
+stim.loc(0.0,sec=axon.axon)
 stim.amp=2
 stim.dur=5
 ena=50
 def setena (en=50):
   global ena
   ena=en
-  for x in soma: x.ena=ena
+  for x in axon.axon: x.ena=ena
 
 def ae ():
   for x,v,i in zip(np.linspace(0,1,len(vvec)),vvec,range(len(vvec))):
-    v.record(soma(x)._ref_v) 
-    nc=h.NetCon(soma(x)._ref_v, None, sec=soma)
+    v.record(axon.axon(x)._ref_v) 
+    nc=h.NetCon(axon.axon(x)._ref_v, None, sec=axon.axon)
     ncl.append(nc)
     nc.threshold=-20
     nc.record(svec,ivec,i+1)
 
 def af (g):
-  h.printf("Velocity: %0.2f m/s;  ",soma.L/(svec[-1] - svec[0])*1e-3)
+  h.printf("Velocity: %0.2f m/s;  ",axon.axon.L/(svec[-1] - svec[0])*1e-3)
   h.printf("Instant speeds: ")
-  print ["%0.2f m/s "%(soma.L/(len(svec)-1)/x*1e-3) for x in np.diff(svec)]
+  print ["%0.2f m/s "%(axon.axon.L/(len(svec)-1)/x*1e-3) for x in np.diff(svec)]
   h.printf(" (ena=%d mV)\n",ena)
   for i,v in enumerate(vvec): v.line(g, h.dt, i+1, 4)
   g.size(0,5,-70,50)
