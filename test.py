663682de75d0e7ef25a143be32bcc0a0da3b05eb
Bill Lytton <billl@neurosim.downstate.edu>
2017-03-10 16:17:16 -0500

    get test.py batchable

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import sys
 import itertools as itr
 import pandas as pd
-params, data = None, None
+import numpy as np
+import pylab as plt
+fig, ax, params, data = None, None, None, None
 
 # reading data section
 def read ():
@@ -23,6 +25,10 @@ def loadpd (filename = 'data/baxA/df4.pd'):
 # df4.query('gnabar==0.12 & temp==6.3 & rall==200').plot('percnajr','V0max',label="abc",ax=ax)
 # df4[(df4.gnabar==0.12) & (df4.temp==6.3) & (df4.rall==200)].plot('percnajr','V0max',label="abc",ax=ax)
 
+def mkfig (): 
+  global fig,ax
+  fig, ax = plt.subplots(1, 1)
+
 def mkqstr (l):
   'Create a query string from a list of tuples with (name, value)'
   st,sr = '',''
@@ -32,39 +38,16 @@ def mkqstr (l):
   return (st.strip('[ &]'),sr.strip('[,]'))
 
 def supfigs ():
-  global res
   ax.clear()
   for i,tup in enumerate([zip(labs,x) for x in itr.product(*[v for x,v in vals.iteritems()])]):
     st,sr=mkqstr(tup)
     res=df4.query(st)
     res.plot('percnajr','V0max',label=sr,ax=ax,linewidth=10-i)
 
-def plotf (dataDir='data/', batchLabel='baxA', params=params, data=data): # from plotfINa
-    import utils
-    utils.setPlotFormat(numColors = 8)
-    Pvals = params[0]['values']
-    Ivals = params[1]['values']
-    Pvalsdic = {val: i for i,val in enumerate(Pvals)}
-    Ivalsdic = {val: i for i,val in enumerate(Ivals)}
-    rates = [[0 for x in range(len(Pvals))] for y in range(len(Ivals))] 
-    for key, d in data.iteritems():
-        rate = len(d['simData']['spkt'])
-        Pindex = Pvalsdic[d['paramValues'][0]]
-        Iindex = Ivalsdic[d['paramValues'][1]]
-        rates[Iindex][Pindex] = rate
-        print d['paramValues']
-        print rate
-
-    filename = '%s/%s/%s_fIcurve.json' % (dataFolder, batchLabel, batchLabel)
-    with open(filename, 'w') as fileObj:
-        json.dump(rates, fileObj)
-
-    plt.figure()
-
-    handles = plt.plot(rates, marker='o', markersize=10)
-    plt.xlabel('Somatic current injection (nA)')
-    plt.xticks(range(len(Ivals))[::2], Ivals[::2])
-    plt.ylabel('Frequency (Hz)')
-    plt.legend(handles, params[0]['values'], title = 'dend Na', loc=2)
-    plt.savefig('%s/%s/%s_fIcurve.png' % (dataFolder, batchLabel, batchLabel))
-    plt.show()
+if __name__ == '__main__':
+  labs=['gnabar', 'rall', 'temp']  # should grab labs and vals from pandas table but need to ident which are params
+  vals={'gnabar': [0.3, 0.12], 'rall': [34.5, 200.0], 'temp': [6.3, 37.0]} 
+  mkfig()
+  df4=loadpd()
+  supfigs()
+  plt.show()
