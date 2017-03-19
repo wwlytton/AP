cac737b09df2858f1222a7ed019d9185a84f5e9c
Bill Lytton <billl@neurosim.downstate.edu>
2017-03-18 21:02:16 -0400

    try to record from some cells; only run for 100 ms

--- netw.py
+++ netw.py
@@ -65,10 +65,11 @@ netParams.connParams['I->E'] = {
 
 # Simulation options
 simConfig = specs.SimConfig()        # object of class SimConfig to store simulation configuration
-simConfig.duration = 1*1e3           # Duration of the simulation, in ms
-simConfig.dt = 0.05                  # Internal integration timestep to use
+simConfig.duration = 100             # Duration of the simulation, in ms
+simConfig.dt = 0.05                  # Internal integration timestep to nuse
 simConfig.verbose = False            # Show detailed messages 
 simConfig.recordTraces = {'V_soma':{'sec':'soma','loc':0.5,'var':'v'}}  # Dict with traces to record
+simConfig.plotCells = ['E2','E4','E5']    # Plot recorded traces for this list of cells
 simConfig.recordStep = 1             # Step size in ms to save data (eg. V traces, LFP, etc)
 simConfig.filename = 'model_output'  # Set file output name
 simConfig.savePickle = False         # Save params, network and sim output to pickle file
