1d0707e963eed48c6fd3c2dce22ede10a553275e
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-25 09:46:41 -0400

    this should be a minimal sim

--- brill77.py
+++ brill77.py
@@ -12,10 +12,17 @@ from collections import OrderedDict
 h.load_file('stdrun.hoc')
 h.tstop = 6
 
-h.load_file('brill77/cable.hoc')  # make(50, 1000) is called at bottom of file
-h.geom(9500) # set long internode interval
+h.load_file('brill77/cable.hoc')  # make(50, 1000) is called at bottom of file; 50 nodes with 1e3 internode interval
 stim=h.IClamp(0.5,sec=h.node[0])
 stim.delay, stim.dur, stim.amp = 0, 0.1, 10
+
+g=h.Graph()
+g.size(0,6,-60,40)
+g.addvar("node0", h.node[0](0.5)._ref_v,1,2)
+g.addvar("node1", h.node[1](0.5)._ref_v,3,2)
+g.addvar("node25", h.node[25](0.5)._ref_v,4,2)
+g.addvar("node49",h.node[49](0.5)._ref_v,2,2)
+h.graphList[0].append(g)
 h.run()
 
 
