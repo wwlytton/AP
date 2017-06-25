e3c884caab287ec4cd29a0e2f1bc846b809d701d
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-25 09:29:14 -0400

    get a spike

--- brill77.py
+++ brill77.py
@@ -9,4 +9,13 @@ import json
 fig, ax, params, data, pdi = None, None, None, None, None
 from netpyne import specs, sim
 from collections import OrderedDict
+h.load_file('stdrun.hoc')
+h.tstop = 6
+
+h.load_file('brill77/cable.hoc')  # make(50, 1000) is called at bottom of file
+h.geom(9500) # set long internode interval
+stim=h.IClamp(0.5,sec=h.node[0])
+stim.delay, stim.dur, stim.amp = 0, 0.1, 10
+h.run()
+
 
