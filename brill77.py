41e6cced1dea23a905c5a2c1f243d6f37b8168aa
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-25 09:49:44 -0400

    needed to import neuron and don't need other stuff for now

--- brill77.py
+++ brill77.py
@@ -1,14 +1,9 @@
 # execfile('test.py')
-import sys
-sys.path.insert(1,'/usr/site/nrniv/local/python/netpyne')
-import itertools as itr
-import pandas as pd
+from neuron import h
+import os, sys, json
 import numpy as np
 import pylab as plt
-import json
-fig, ax, params, data, pdi = None, None, None, None, None
-from netpyne import specs, sim
-from collections import OrderedDict
+
 h.load_file('stdrun.hoc')
 h.tstop = 6
 
@@ -17,7 +12,7 @@ stim=h.IClamp(0.5,sec=h.node[0])
 stim.delay, stim.dur, stim.amp = 0, 0.1, 10
 
 g=h.Graph()
-g.size(0,6,-60,40)
+g.size(0, h.tstop, -80, 50)
 g.addvar("node0", h.node[0](0.5)._ref_v,1,2)
 g.addvar("node1", h.node[1](0.5)._ref_v,3,2)
 g.addvar("node25", h.node[25](0.5)._ref_v,4,2)
