1ed6ad271f151a85b1ab4cea31de1da876ac90c0
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-26 16:17:03 -0400

    replace the xopen calls

--- Cav32.py
+++ Cav32.py
@@ -1,4 +1,3 @@
-# execfile('test.py')
 from neuron import h
 import os, sys, json
 import numpy as np
@@ -10,5 +9,5 @@ datestr = os.popen('datestring').read()
 
 # just work with 3 compartment cell for now re3_cc, also re1 and re80, and reD (dissociated cell)
 # have different demos in Cav32_WT (wild type); Cav32_R788C; Cav32_C456S
-
+h.xopen('Cav32RE3cc.hoc')
 
