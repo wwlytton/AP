9225097d10cc54bc8b453fa83e7b9b74b8d46a99
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-26 17:55:48 -0400

    make the list of it2s available as it2l

--- Cav32.py
+++ Cav32.py
@@ -6,6 +6,7 @@ import pickle as pkl
 h.load_file('stdrun.hoc')
 fig, axi = None, None
 datestr = os.popen('datestring').read()
+it2l = ['it2WT', 'it2C456S', 'it2R788C']
 
 # just work with 3 compartment cell for now re3_cc, also re1 and re80, and reD (dissociated cell)
 # have different demos in Cav32_WT (wild type); Cav32_R788C; Cav32_C456S
@@ -19,11 +20,11 @@ def setup ():
   h.soma[0].insert('hh2nafjr')
   h.soma[0].gnabar_hh2nafjr = 0.0
   for sec in seclist: 
-    for mech in ['it2WT', 'it2C456S', 'it2R788C']: 
+    for mech in it2l:
       sec.insert(mech)
       h('%s.gcabar_%s = 0.0'%(str(sec),mech))
 
-def setparams (it2='it2WT', pnafjr=0.0, gnabar=0.1):
+def setparams (it2=it2l[0], pnafjr=0.0, gnabar=0.1):
   h.soma[0].gnabar_hh2nafjr = pnafjr *gnabar
   h.soma[0].gnabar_hh2  =  (1-pnafjr)*gnabar
   h('soma.gcabar_%s = %g'%(it2, 4.5e-5)) #  h.soma[0](0.5).it2WT.gcabar
