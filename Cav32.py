a5e53494203933657f8228a88b93da8b1cc8d2c6
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-26 16:47:36 -0400

    insert the WT chan and now get a burst

--- Cav32.py
+++ Cav32.py
@@ -11,3 +11,11 @@ datestr = os.popen('datestring').read()
 # have different demos in Cav32_WT (wild type); Cav32_R788C; Cav32_C456S
 h.xopen('Cav32RE3cc.hoc')
 
+# need a Ca channel -- start with WT
+seclist = h.SectionList()
+seclist.wholetree()
+def insertcav (it2='it2WT'):
+  for sec in seclist: sec.insert(it2)
+  h('soma.gcabar_%s = %g'%(it2, 4.5e-5)) #  h.soma[0](0.5).it2WT.gcabar
+  h('dend1[0].gcabar_%s = %g'%(it2, h.corrD*4.5e-5))
+  h('dend1[1].gcabar_%s = %g'%(it2, h.corrD*6.8e-4))
