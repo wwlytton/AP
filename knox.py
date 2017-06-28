195b389636658bdeddb4bb018e63b29d3769663c
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-28 16:04:39 -0400

    convert everything over to dict -- all loads anyway

--- knox.py
+++ knox.py
@@ -13,47 +13,47 @@ datestr = os.popen('datestring').read()
 
 h.load_file('Fspikewave.oc')
 
-def mksecls (): 
-  global TCsl, REsl, PYsl, INsl, thalDict
-  TCsl, REsl, PYsl, INsl = [],[],[],[]
+def mkdict (): 
+  tD = {'TC': {'cel': [], 'gnabar': h.TC[0].soma[0].gnabar_hh2, 'ncl': []},
+        'RE': {'cel': [], 'gnabar': h.RE[0].soma[0].gnabar_hh2, 'ncl': []},
+        'PY': {'cel': [], 'gnabar': h.PY[0].soma[0].gnabar_hh2, 'ncl': []},
+        'IN': {'cel': [], 'gnabar': h.TC[0].soma[0].gnabar_hh2, 'ncl': []}}
   for i in range(int(h.nthalamiccells)):
-    TCsl.append(h.TC[i]) 
-    REsl.append(h.RE[i])
-    PYsl.append(h.PY[i])
-    INsl.append(h.IN[i]) 
-  thalDict = {'TC': {'sl': TCsl, 'gnabar': TCsl[0].soma[0].gnabar_hh2, 'ncl': []},
-              'RE': {'sl': REsl, 'gnabar': REsl[0].soma[0].gnabar_hh2, 'ncl': []},
-              'PY': {'sl': PYsl, 'gnabar': PYsl[0].soma[0].gnabar_hh2, 'ncl': []},
-              'IN': {'sl': INsl, 'gnabar': TCsl[0].soma[0].gnabar_hh2, 'ncl': []}}
-  for tyl in thalDict.values():
-    for i,ce in enumerate(tyl['sl']):
+    tD['TC']['cel'].append(h.TC[i]) 
+    tD['RE']['cel'].append(h.RE[i])
+    tD['PY']['cel'].append(h.PY[i])
+    tD['IN']['cel'].append(h.IN[i]) 
+  for tyl in tD.values():
+    for i,ce in enumerate(tyl['cel']):
       ncl = h.cvode.netconlist(ce,'','')
       if len(ncl)>0: tyl['ncl'].append(ncl[0]) # just take one
       else: print 'No netcons found for cell %s'%str(ce)
+  return tD
 
 def setup ():
   h.tstop=1e3
-  for ty in [TCsl, REsl, PYsl, INsl]:
-    for ce in ty:
+  for vals in thalDict.values():
+    for ce in vals['cel']:
       ce.soma[0].insert('hh2nafjr')
       ce.soma[0].gnabar_hh2nafjr = 0.0
       
 def setparams (pnafjr=0.0, gnamult=1.0, tyli=['TC', 'RE', 'PY', 'IN']):
-  for ty in tyli:
-    for ce in thalDict[ty]['sl']:
-      ce.soma[0].gnabar_hh2nafjr = pnafjr *  gnamult * thalDict[ty]['gnabar']
-      ce.soma[0].gnabar_hh2  =  (1-pnafjr) * gnamult * thalDict[ty]['gnabar']
+  for vals in thalDict.values():
+    for ce in vals['cel']:
+      ce.soma[0].gnabar_hh2nafjr = pnafjr *  gnamult * vals['gnabar']
+      ce.soma[0].gnabar_hh2  =  (1-pnafjr) * gnamult * vals['gnabar']
 
 # recording
 def recv (thresh=-5):
-  global spkt,spkid,ncl,nrec
-  tyli=['TC', 'RE', 'PY', 'IN']
-  spkt, spkid = h.Vector(1e3), h.Vector(1e3)
-  for nc in ncl: nc.threshold=thresh
-  nrec = [h.Vector(h.tstop/h.dt+10) for x in range(len(nodl))]
-  for i,nc in enumerate(ncl): nc.record(spkt,spkid, i)  # netcon.record(tvec, idvec, id)
-  for v,n in zip(nrec,nodl): v.record(n(0.5)._ref_v)
+  global spkt,spkid,nrec
+  spkt, spkid, nrec = h.Vector(1e3), h.Vector(1e3), []
+  for k,v in thalDict.iteritems():
+    for i, (ce, nc) in enumerate(zip(v['cel'],v['ncl'])):
+      ve = h.Vector(h.tstop/h.dt +10)
+      ve.record(ce.soma[0](0.5)._ref_v)
+      nrec.append(ve)
+      nc.record(spkt, spkid, i)
 
-mksecls()
+thalDict = mkdict()
 setup()
 setparams()
