85be5a96fc556d9e24df87f2423a5a22cdf535a1
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-28 08:58:40 -0400

    made same mistake of copying lines and not editing them

--- knox.py
+++ knox.py
@@ -17,22 +17,22 @@ def mksecls ():
     PYsl.append(h.PY[i])
     INsl.append(h.IN[i]) 
   thalDict = {'TC': (TCsl, TCsl[0].soma[0].gnabar_hh2), 
-              'RE': (TCsl, REsl[0].soma[0].gnabar_hh2),
-              'PY': (TCsl, PYsl[0].soma[0].gnabar_hh2),
+              'RE': (REsl, REsl[0].soma[0].gnabar_hh2),
+              'PY': (PYsl, PYsl[0].soma[0].gnabar_hh2),
               'IN': (INsl, TCsl[0].soma[0].gnabar_hh2)}
 
 def setup ():
-  h.tstop=2e3
+  h.tstop=1e3
   for ty in [TCsl, REsl, PYsl, INsl]:
     for ce in ty:
       ce.soma[0].insert('hh2nafjr')
       ce.soma[0].gnabar_hh2nafjr = 0.0
       
-def setparams (pnafjr=0.0, gnamult=1.0, tyli=['RE']):
+def setparams (pnafjr=0.0, gnamult=1.0, tyli=['RE', 'TC', 'PY', 'IN']):
   for ty in tyli:
     for ce in thalDict[ty][0]:
-      ce.soma[0].gnabar_hh2nafjr = pnafjr *thalDict[ty][1]
-      ce.soma[0].gnabar_hh2  =  (1-pnafjr)*thalDict[ty][1]
+      ce.soma[0].gnabar_hh2nafjr = pnafjr *  gnamult * thalDict[ty][1]
+      ce.soma[0].gnabar_hh2  =  (1-pnafjr) * gnamult * thalDict[ty][1]
 
 mksecls()
 setup()
