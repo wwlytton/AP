07da2da7c24710fbc1316e8bbfe2b0cf6e7392b0
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-28 07:38:27 -0400

    iterate the lists to put in the alternative naf and to then do the param settings

--- knox.py
+++ knox.py
@@ -11,20 +11,21 @@ def mksecls ():
   global TCsl, REsl, PYsl, INsl
   TCsl, REsl, PYsl, INsl = [h.SectionList() for i in range(4)]
   for i in range(int(h.nthalamiccells)):
-    TCsl.append(h.IN[i].soma[0]) 
-    REsl.append(h.IN[i].soma[0])
-    PYsl.append(h.IN[i].soma[0])
-    INsl.append(h.IN[i].soma[0]) 
+    TCsl.append(h.IN[i]) 
+    REsl.append(h.IN[i])
+    PYsl.append(h.IN[i])
+    INsl.append(h.IN[i]) 
 
 def setup ():
-  if False:
-    h.soma[0].insert('hh2nafjr')
-    h.soma[0].gnabar_hh2nafjr = 0.0
+  for ty in mksecls:
+    for ce in ty:
+      ce.soma[0].insert('hh2nafjr')
+      ce.soma[0].gnabar_hh2nafjr = 0.0
 
-def setparams (pnafjr=0.0, gnabar=0.1):
-  if False:
-    h.soma[0].gnabar_hh2nafjr = pnafjr *gnabar
-    h.soma[0].gnabar_hh2  =  (1-pnafjr)*gnabar
+def setparams (pnafjr=0.0, gnabar=0.1, cli='REsl'):
+  for ce in cli:
+    ce.soma[0].gnabar_hh2nafjr = pnafjr *gnabar
+    ce.soma[0].gnabar_hh2  =  (1-pnafjr)*gnabar
 
 mksecls()
 setup()
