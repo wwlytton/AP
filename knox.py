6a7d0311cadf857e784154162151676066cd04b9
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-27 22:35:09 -0400

    make sectionlists for the different types of cells

--- knox.py
+++ knox.py
@@ -3,14 +3,20 @@ import os, sys, json
 import numpy as np
 import pylab as plt
 import pickle as pkl
-h.load_file('stdrun.hoc')
 fig, axi = None, None
 datestr = os.popen('datestring').read()
 
-h.load_file('Knox_Absence_model/Fspikewave.oc')
+h.load_file('Fspikewave.oc')
+def mksecls (): 
+  global TCsl, REsl, PYsl, INsl
+  TCsl, REsl, PYsl, INsl = [h.SectionList() for i in range(4)]
+  for i in range(int(h.nthalamiccells)):
+    TCsl.append(h.IN[i].soma[0]) 
+    REsl.append(h.IN[i].soma[0])
+    PYsl.append(h.IN[i].soma[0])
+    INsl.append(h.IN[i].soma[0]) 
 
 def setup ():
-  h.trans=0 # this means don't read from SaveState
   if False:
     h.soma[0].insert('hh2nafjr')
     h.soma[0].gnabar_hh2nafjr = 0.0
@@ -20,6 +26,7 @@ def setparams (pnafjr=0.0, gnabar=0.1):
     h.soma[0].gnabar_hh2nafjr = pnafjr *gnabar
     h.soma[0].gnabar_hh2  =  (1-pnafjr)*gnabar
 
+mksecls()
 setup()
 setparams()
 
