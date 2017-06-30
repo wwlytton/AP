3fe90f5ca20a05f2ed15ecc4a836d1dfd42fc204
Bill Lytton <billl@neurosim.downstate.edu>
2017-06-30 15:32:51 -0400

    whoops this makes no sense

--- basicHH.py
+++ basicHH.py
@@ -21,18 +21,18 @@ soma.insert('nafjr')
 soma(0.5).nafjr.gnabar
 soma.gnabar_nafjr=0
 stim = h.IClamp(soma(0.5))
-stim.dur, stim.delay, stim.amp = 10, 2, 0
-h.tstop=50
+stim.delay, stim.dur, stim.amp = 0, 5, 5
+h.tstop=10
 fig, axi = plt.subplots(1, 1)
-axi.clear()
 tvec=h.Vector()
 vecl=[h.Vector()]
 tvec.record(h._ref_t)
 vecl[0].record(soma(0.5)._ref_v)
 
 # sim runs
-# setparams(0.0)
+axi.clear()
+setparams(0.0)
 h.run()
 plt.plot(tvec,vecl[0],color='red',linewidth=5)
-# setparams(1.0)
-# plt.plot(tvec,vecl[0],color='blue',linewidth=5)
+setparams(1.0)
+plt.plot(tvec,vecl[0],color='blue',linewidth=5)
