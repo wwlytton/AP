b889aeecb54e3ccc30b814329372d9a53d52c4bb
Bill Lytton <billl@neurosim.downstate.edu>
2017-07-01 09:04:49 -0400

    erase the axes and put in scale bars

--- graph.py
+++ graph.py
@@ -18,4 +18,9 @@ def replot (d, w=2, col='b'):
   if len(tvec)!=len(d[0][1]): tvec=h.Vector(len(d[0][1])).indgen(h.dt)
   for ax in axi: ax.clear()
   for ax in axi: ax.set_ylim(-120,50) 
-  for ax, d1 in zip(axi, d): ax.plot(tvec, d1[1], linewidth=w, color=col)
+  for ax, d1 in zip(axi, d): 
+    ax.plot(tvec, d1[1], linewidth=w, color=col)
+    ax.set_axis_off()
+  plt.vlines(600, 0, 50)
+  plt.hlines(0, 550, 600)
+
