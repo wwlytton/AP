cf209eedc522bc94fb2ceca25651138d988db8c7
Bill Lytton <billl@neurosim.downstate.edu>
2017-07-01 10:01:12 -0400

    try to get things into correct column by doing a transpose on the axis list

--- graph.py
+++ graph.py
@@ -10,16 +10,18 @@ import pickle as pkl
 tvec=[]
 
 def mkfig ():
-  global fig,axi
-  fig, axi = plt.subplots(6, 1)
+  global fig, axi0, axi
+  fig, axi0 = plt.subplots(6, 3)
+  axit = zip(*axi) # transpose
 
-def replot (d, w=2, col='b'):
+def replot (d, w=2, column=0, color='b'):
   global tvec
+  axi=axit[column]
   if len(tvec)!=len(d[0][1]): tvec=h.Vector(len(d[0][1])).indgen(h.dt)
   for ax in axi: ax.clear()
   for ax in axi: ax.set_ylim(-120,50) 
   for ax, d1 in zip(axi, d): 
-    ax.plot(tvec, d1[1], linewidth=w, color=col)
+    ax.plot(tvec, d1[1], linewidth=w, color=color)
     ax.set_axis_off()
     # https://matplotlib.org/users/text_props.html
     ax.text(0, 0, '%d%%'%d1[0],fontsize=14, ha='center', va='top')
