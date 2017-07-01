9215fbd3fe4ec35783373624c0f7025aa50de015
Bill Lytton <billl@neurosim.downstate.edu>
2017-07-01 08:54:02 -0400

    draw a graphic

--- graph.py
+++ graph.py
@@ -1,15 +1,21 @@
 '''
 USAGE:
 '''
+from neuron import h
+import os, sys, json
+import numpy as np
+import pylab as plt
+import pickle as pkl
 
-tvec=None
+tvec=[]
 
 def mkfig ():
   global fig,axi
   fig, axi = plt.subplots(6, 1)
 
 def replot (d, w=2, col='b'):
-  if not tvec: tvec=h.Vector(len(d[0][1])).indgen(h.dt)
+  global tvec
+  if len(tvec)!=len(d[0][1]): tvec=h.Vector(len(d[0][1])).indgen(h.dt)
   for ax in axi: ax.clear()
   for ax in axi: ax.set_ylim(-120,50) 
   for ax, d1 in zip(axi, d): ax.plot(tvec, d1[1], linewidth=w, color=col)
