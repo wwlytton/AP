df0e5adf2a6515f777f449ff51c6a01baaedc9a1
Bill Lytton <billl@neurosim.downstate.edu>
2017-07-01 08:49:59 -0400

    bring in graph.py as g

--- graph.py
+++ graph.py
@@ -2,13 +2,14 @@
 USAGE:
 '''
 
+tvec=None
+
 def mkfig ():
   global fig,axi
   fig, axi = plt.subplots(6, 1)
 
-tvec=h.Vector(len(d[0][1])).indgen(h.dt)
-
-def replot (w=2, col='b'):
+def replot (d, w=2, col='b'):
+  if not tvec: tvec=h.Vector(len(d[0][1])).indgen(h.dt)
   for ax in axi: ax.clear()
   for ax in axi: ax.set_ylim(-120,50) 
   for ax, d1 in zip(axi, d): ax.plot(tvec, d1[1], linewidth=w, color=col)
