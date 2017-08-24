65ac0a128b6e8cf92de9e522c74850cdb52f9db8
Bill Lytton <billl@neurosim.downstate.edu>
2017-08-24 13:16:18 -0400

    fix the bugs

--- channelpediamod/test.py
+++ channelpediamod/test.py
@@ -1,17 +1,17 @@
+#!/usr/bin/python
 import subprocess32 as subp
+import pickle as pkl
 import re
-chanlist,cdi,nadi,kdi = pkl.load(open(fi)) # read back in with load()
-dr = '/u/billl/nrniv/AP/channelpediamod/'
+chanlist,cdi,nadi,kdi = pkl.load(open('channels.pkl')) # read back in with load()
 
-for k,v in chanlist:
+for k,v in cdi.iteritems():
   for mf in v[1]: 
-    fi = dr+'%s.mod'%mf
-    gr = subp.check_output(['grep', 'SUFFIX', fi]).split()
+    fi = '%s.mod'%mf
+    gr = subp.check_output(['grep', '-n', 'SUFFIX', fi]).split()
     if (len(gr)!=3): raise(Exception('grep SUFFIX on %s returned %s'%(fi,gr)))
     lnum, suff = int(re.findall('\d+',gr[0])[0])-1, gr[2] # line# now 0 offset statt 1
-    fpi, fpo = map(open, [fi, dr+'%s%s.mod'%(suff,mf)], ['r','w'])
+    fpi, fpo = map(open, [fi, '%s%s.mod'%(suff,mf)], ['r','w'])
     li=fpi.readlines()
     lin=re.sub('SUFFIX (.+_.+)','SUFFIX \\1_%s'%mf,li[lnum])
     print lin
-    map(close, [fpi, fpo])
-    
+    fpi.close(); fpo.close()
