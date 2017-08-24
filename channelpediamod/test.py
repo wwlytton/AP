b0572e5b498219a2df9d52dcdd29ea3411abda1b
Bill Lytton <billl@neurosim.downstate.edu>
2017-08-24 13:06:10 -0400

    grep and read file

--- channelpediamod/test.py
+++ channelpediamod/test.py
@@ -1,6 +1,17 @@
+import subprocess32 as subp
+import re
 chanlist,cdi,nadi,kdi = pkl.load(open(fi)) # read back in with load()
+dr = '/u/billl/nrniv/AP/channelpediamod/'
 
 for k,v in chanlist:
   for mf in v[1]: 
-    fi = '/u/billl/nrniv/AP/channelpediamod/' + '%s.mod'%mf
-    try: open
+    fi = dr+'%s.mod'%mf
+    gr = subp.check_output(['grep', 'SUFFIX', fi]).split()
+    if (len(gr)!=3): raise(Exception('grep SUFFIX on %s returned %s'%(fi,gr)))
+    lnum, suff = int(re.findall('\d+',gr[0])[0])-1, gr[2] # line# now 0 offset statt 1
+    fpi, fpo = map(open, [fi, dr+'%s%s.mod'%(suff,mf)], ['r','w'])
+    li=fpi.readlines()
+    lin=re.sub('SUFFIX (.+_.+)','SUFFIX \\1_%s'%mf,li[lnum])
+    print lin
+    map(close, [fpi, fpo])
+    
