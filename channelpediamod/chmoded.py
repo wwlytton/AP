0e42df17c1fbe230aa45f85fbb037362172cad72
Bill Lytton <billl@neurosim.downstate.edu>
2017-08-24 13:33:50 -0400

    some difficulty -- can't find 211.mod

--- channelpediamod/chmoded.py
+++ channelpediamod/chmoded.py
@@ -5,13 +5,12 @@ import re
 chanlist,cdi,nadi,kdi = pkl.load(open('channels.pkl')) # read back in with load()
 
 for k,v in cdi.iteritems():
-  for mf in v[1]: 
+  for mf in v: 
     fi = '%s.mod'%mf
     gr = subp.check_output(['grep', '-n', 'SUFFIX', fi]).split()
     if (len(gr)!=3): raise(Exception('grep SUFFIX on %s returned %s'%(fi,gr)))
     lnum, suff = int(re.findall('\d+',gr[0])[0])-1, gr[2] # line# now 0 offset statt 1
     fpi, fpo = map(open, [fi, '%s%s.mod'%(suff,mf)], ['r','w'])
     li=fpi.readlines()
-    lin=re.sub('SUFFIX (.+_.+)','SUFFIX \\1_%s'%mf,li[lnum])
-    print lin
+    li[lnum] = re.sub('SUFFIX (.+_.+)','SUFFIX \\1_%s'%mf,li[lnum])
     fpi.close(); fpo.close()
