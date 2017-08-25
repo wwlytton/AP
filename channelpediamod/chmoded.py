f9849a73009cffd5817285c3f5c465e5a8f26517
Bill Lytton <billl@neurosim.downstate.edu>
2017-08-25 14:47:52 -0400

    use the name out of the chdb instead of the name in the SUFFIX as name of the file

--- channelpediamod/chmoded.py
+++ channelpediamod/chmoded.py
@@ -10,7 +10,10 @@ for k,v in cdi.iteritems():
     gr = subp.check_output(['grep', '-n', 'SUFFIX', fi]).split()
     if (len(gr)!=3): raise(Exception('grep SUFFIX on %s returned %s'%(fi,gr)))
     lnum, suff = int(re.findall('\d+',gr[0])[0])-1, gr[2] # line# now 0 offset statt 1
-    fpi, fpo = map(open, [fi, '%s_%s.mod'%(suff,mf)], ['r','w'])
+    cod = suff.split('_')
+    if cod[-1].isdigit() and int(cod[-1]) != int(v[0]): print('Code# discrepancy: %d %d for %s.mod: %s'%int(cod[-1]), int(v[0]), mf, suff)
+    fo = '%s_%04d_%s.mod'%(k,int(v[0]),mf)
+    fpi, fpo = map(open, [fi, fo], ['r','w'])
     li=fpi.readlines()
     li[lnum] = re.sub('SUFFIX (.+_.+)','SUFFIX \\1_%s'%mf,li[lnum])
     fpo.writelines(li)
