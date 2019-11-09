ac2bd0e13916235b6bd6784ecf23e3b3d2c0a1d9
Bill Lytton <neuro014@gmail.com>
2019-11-08 20:12:13 -0500

    going back to 722e6b16bbf2d342514540eef4ecf8245b55e838 when had the axon in netParams instead of being separate file

--- netParams.py
+++ netParams.py
@@ -1,12 +1,17 @@
 from netpyne import specs
-from netpyne.specs import Dict, ODict
-from cfg import cfg
-
+try:
+	from __main__ import cfg  # import SimConfig object with params from parent module
+except:
+	from cfg import cfg
+	
 netParams = specs.NetParams()   # object of class NetParams to store the network parameters
-axr=netParams.importCellParams(label='axA', conds={'cellType': 'axA'}, fileName='axonA.py', cellName='AxonA', cellArgs={'percnajr':cfg.percnajr, 'rall':cfg.rall, 'gnabar':cfg.gnabar})
-netParams.popParams.axA = Dict({'cellType': 'axA', 'numCells': 1})
+cellRule=netParams.importCellParams(label='axA', conds={'cellType': 'axA'}, fileName='axonA.py', cellName='AxonA', 
+                               cellArgs={'percnajr':0.5, 'rall':34.5, 'gnabar':0.12})
+netParams.popParams.axA = {'cellType': 'axA', cellModel: 'HH', 'numCells': 1}
+
+axr.secs.axon.ions.na.e = cfg.percnajr
+axr.secs.axon.mechs.hh.gnabar = cfg.gnabar
 
 ic = cfg.stim
-netParams.stimSourceParams.stim = Dict({'type': 'IClamp', 'delay': 0, 'dur': ic['dur'], 'amp': ic['amp']})
-netParams.stimTargetParams.stimaxon = Dict({'source': 'stim', 'conds': {'popLabel': ic['popu']}, 'sec': ic['sec'], 'loc': ic['loc']})
-
+netParams.stimSourceParams.stim = {'type': 'IClamp', 'delay': 0, 'dur': ic['dur'], 'amp': ic['amp']}
+netParams.stimTargetParams.stimaxon = {'source': 'stim', 'conds': {'popLabel': ic['popu']}, 'sec': ic['sec'], 'loc': ic['loc']}
