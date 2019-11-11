8db75e2fabfb48b7682f47e80fb04d897d3f8364
Bill Lytton <neuro014@gmail.com>
2019-11-11 12:27:19 -0500

    loads and runs

--- netParams.py
+++ netParams.py
@@ -7,7 +7,7 @@ netParams.popParams.axA = {'cellType': 'axA', 'cellModel': 'ax', 'numCells': 1}
 netParams.cellParams['PYRrule'] = {'conds':{'cellType': 'axA', 'cellModel': 'ax'},
    'secs': {'axon': {'geom': {'diam': 5, 'L': 2000, 'Ra': 123.0}, 
                      'ions': {'k': {'e': -77.0}, 'na': {'e': 50.0}},
-                     'mechs':{'ina2005': {'gnatbar': 0.3}, 'ik2005': {'gkbar': 0.3}, 'pas': {'g': 0.0001, 'e': -70.0}}}}}
+                     'mechs':{'ina2005': {'gnatbar': 0.3}, 'ik2005': {'gkfbar': 0.3}, 'pas': {'g': 0.0001, 'e': -70.0}}}}}
 
 # cellRule=netParams.importCellParams(label='axA', conds={'cellType': 'axA'}, fileName='axonA.py', cellName='AxonA', cellArgs={'percnajr':0.5, 'rall':34.5, 'gnabar':0.12})
 # axr.secs.axon.ions.na.e = cfg.percnajr
