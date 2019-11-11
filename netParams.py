5e41b1818adfbe72fb9f2320096dc1a037397e38
Bill Lytton <neuro014@gmail.com>
2019-11-11 10:55:48 -0500

    not quite: 'Creating network of 0 cell populations on 1 hosts...'

--- netParams.py
+++ netParams.py
@@ -2,7 +2,7 @@ from netpyne import specs
 from cfg import cfg
 	
 netParams = specs.NetParams()   # object of class NetParams to store the network parameters
-netParams.popParams.axA = {'cellType': 'axA', 'cellModel': 'HH', 'numCells': 1}
+netParams.popParams.axA = {'cellType': 'axA', 'cellModel': 'axA', 'numCells': 1}
 netParams.cellParams['PYRrule'] = {'conds': {'cellType': 'axA'}, 'secs': {'axon': {'geom': {'diam': 18.8, 'L': 18.8, 'Ra': 123.0}, 
                                                                                    'mechs': {'hh': {'gnabar': 0.12, 'gkbar':0.036, 'gl': 0.003, 'el': -70}}}}}
 # cellRule=netParams.importCellParams(label='axA', conds={'cellType': 'axA'}, fileName='axonA.py', cellName='AxonA', cellArgs={'percnajr':0.5, 'rall':34.5, 'gnabar':0.12})
