ddc7df7d5f6bee66cf9a7b2cbc17f8d32fd8d2cb
Bill Lytton <neuro014@gmail.com>
2019-11-11 12:20:31 -0500

    fixed syntax errors from embedded dicts

--- netParams.py
+++ netParams.py
@@ -1,10 +1,14 @@
+# from cfg import simConfig
 from netpyne import specs
 from cfg import cfg
 	
 netParams = specs.NetParams()   # object of class NetParams to store the network parameters
-netParams.popParams.axA = {'cellType': 'axA', 'cellModel': 'axA', 'numCells': 1}
-netParams.cellParams['PYRrule'] = {'conds': {'cellType': 'axA'}, 'secs': {'axon': {'geom': {'diam': 18.8, 'L': 18.8, 'Ra': 123.0}, 
-                                                                                   'mechs': {'hh': {'gnabar': 0.12, 'gkbar':0.036, 'gl': 0.003, 'el': -70}}}}}
+netParams.popParams.axA = {'cellType': 'axA', 'cellModel': 'ax', 'numCells': 1}
+netParams.cellParams['PYRrule'] = {'conds':{'cellType': 'axA', 'cellModel': 'ax'},
+   'secs': {'axon': {'geom': {'diam': 5, 'L': 2000, 'Ra': 123.0}, 
+                     'ions': {'k': {'e': -77.0}, 'na': {'e': 50.0}},
+                     'mechs':{'ina2005': {'gnatbar': 0.3}, 'ik2005': {'gkbar': 0.3}, 'pas': {'g': 0.0001, 'e': -70.0}}}}}
+
 # cellRule=netParams.importCellParams(label='axA', conds={'cellType': 'axA'}, fileName='axonA.py', cellName='AxonA', cellArgs={'percnajr':0.5, 'rall':34.5, 'gnabar':0.12})
 # axr.secs.axon.ions.na.e = cfg.percnajr
 # axr.secs.axon.mechs.hh.gnabar = cfg.gnabar
