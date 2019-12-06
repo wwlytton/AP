62fcd8815d00bb51ef3669453be94e9bd991f395
Bill Lytton <neuro014@gmail.com>
2019-12-06 10:33:19 -0500

    go thru a loop of different percs

--- init.py
+++ init.py
@@ -1,7 +1,9 @@
-from netpyne import sim
+exe('simConfig.py')
+exe('netParams.py')
+sim.create(npar, cfg)
+ax = sim.net.cells[0].secs.axon.hObj
+for perc in np.linspace(0, 0.8, 0.1):
+  h.perc_ina2005 = perc
+  sim.runSim()
+  sim.saveData(include = ['simData'], filename = '19dec06%d'%(100*perc))
 
-from cfg import cfg
-cfg, netParams = sim.readCmdLineArgs()	
-sim.create(simConfig = cfg, netParams = netParams)
-sim.simulate()
-sim.analyze()
