4a70a3c6b9acb6beb272ff82f6fd26bd3104fc45
Bill Lytton <neuro014@gmail.com>
2019-12-07 15:12:46 -0500

    reversed sense of block

--- init.py
+++ init.py
@@ -9,9 +9,9 @@ sim.create(npar, cfg)
 ax = sim.net.cells[0].secs.axon.hObj
 def lp ():
     for perc in np.linspace(0.0, 0.8, 9): # now is percent block
-        ax.gnablock_ina2005 = 1-perc
+        h.gnablock_ina2005 = perc # h.perc_ina2005
         sim.runSim()
         sim.gatherData()
-        sim.saveData(include = ['simData'], filename = 'data/19dec06blk_%02d'%(100*perc))
+        sim.saveData(include = ['simData'], filename = 'data/19dec06blkA_%02d'%(100*perc))
 
 lp()
