b647c533ffe4f22126bd09dfb532fa10a04f3e5e
Bill Lytton <neuro014@gmail.com>
2019-12-07 12:36:27 -0500

    plotlast()

--- test.py
+++ test.py
@@ -7,6 +7,13 @@ def plot1 (ke):
   da=dat1[ke]; tt=np.array(da['t'])
   [plt.plot(tt,v['cell_0']) for k,v in da.items() if 'V_axon' in k]
 
+def plotlast (): 
+  ''' plot last spk from each'''
+  plt.clf()
+  for k,v in dat1.items():
+    da=v['V_axon_1.00']['cell_0']
+    if max(da) > -20: plt.plot(tt,v['V_axon_1.00']['cell_0'])
+
 # Batch run using lp() routine in init.py
 def loadall (files = 'data/19dec06_*.json'): 
   import glob
